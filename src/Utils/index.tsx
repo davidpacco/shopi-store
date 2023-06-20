11cfb36ee908aaf8f7aab020f8af416f88e4ed9e
davidpacco <david.j.pacco@gmail.com>
2023-06-20 18:38:56 -0500

    Data type error fixed

--- src/Utils/index.tsx
+++ src/Utils/index.tsx
@@ -1,6 +1,6 @@
-import { CartProductsDataType } from "../Interfaces/Interfaces"
+import { CartProductsData } from "../Interfaces/Interfaces"
 
-export const getTotal = (products: CartProductsDataType[]) => {
+export const getTotal = (products: CartProductsData[]) => {
   let sum = 0
   for (let i = 0; i < products.length; i++) sum += (products[i].price * products[i].quantity)
   return sum
